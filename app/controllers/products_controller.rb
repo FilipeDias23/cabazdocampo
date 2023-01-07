beebeaeb8941bdc891b04b4c31c4d67da5a079b8
filipe dias <filipediasit@gmail.com>
2023-01-07 19:53:55 +0000

    created products controller records

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,35 @@
 class ProductsController < ApplicationController
   def home
   end
+before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product, notice: 'Product was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :quantity, :type, :price, :available)
+    end
+end
 end
